c1dc48b85a9de2db9c37b974507b7edf84b0f50b
nico santo <nico.santomartino@gmail.com>
2017-06-14 18:13:42 -0300

    version2

--- app/src/main/java/nicosanto/a8proyecto/SegundaActivity.java
+++ app/src/main/java/nicosanto/a8proyecto/SegundaActivity.java
@@ -1,15 +1,19 @@
 package nicosanto.a8proyecto;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class SegundaActivity extends AppCompatActivity {
 
     private String nombre="";
+    private EditText edtTelefono;
     private Button btnMarcar;
     private Button btnFinalizar;
 
@@ -26,7 +30,7 @@ public class SegundaActivity extends AppCompatActivity {
         btnMarcar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(SegundaActivity.this, "boton marcar", Toast.LENGTH_LONG).show(); //Bandera
+                //Toast.makeText(SegundaActivity.this, "boton marcar", Toast.LENGTH_LONG).show(); //Bandera
                 //LLamo a la funcion
                 marcarLLAMADA();
             }
@@ -67,6 +71,9 @@ public class SegundaActivity extends AppCompatActivity {
         // Se debe incluir un botón para marcar la llamada, según el numero de teléfono que ingreso el usuario,
         // para lo cual se debe desarrollar un método de usuario a tal efecto y enlazado al evento click del boton.
         //Intent Implicito - CALL_DIAL
+        edtTelefono = (EditText) findViewById(R.id.edtTelefono);
+        Intent itn = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+edtTelefono));
+        startActivity(itn);
 
     }
 
